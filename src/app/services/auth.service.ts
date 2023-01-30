ac58768475a5e22f8cbae0c425fcd5987555b18d
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2023-01-30 15:38:21 -0600

    variable isLogged

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,32 +1,62 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
 import { User, UserResponse } from '../interfaces/user.interfaces';
+import { JwtHelperService } from '@auth0/angular-jwt';
+const helper = new JwtHelperService();
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
+
+
+export class AuthService {
+ private loggedIn = new BehaviorSubject<boolean>(false);
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.checkToken();
+  }
+
+get isLogged():Observable<boolean> {
+  return this.loggedIn.asObservable();
+}
 
 login(authData: User): Observable<UserResponse | void>{
   return this.http.post<UserResponse>(`${environment.API_URL}/auth/login`, authData)
   .pipe(
     map( (res: UserResponse)=> {
       console.log('Res->',res);
-      //savenToken()
+      this.saveToken(res.token)
+      this.loggedIn.next(true);
     }),
     catchError((err) => this.handlerError(err))
   );
 }
 
-logout(): void {}
+logout(): void {
+  localStorage.removeItem('token')
+  this.loggedIn.next(false);
+}
 
-private readToken(): void {}
-private saveToken(): void {}
+private checkToken(): void {
+const userToken = localStorage.getItem('token');
+const isExpired =  helper.isTokenExpired(userToken)
+console.log('isExpired', isExpired);
+
+ if (isExpired) {
+  this.logout();
+} else {
+  this.loggedIn.next(true)
+}
+
+}
+
+
+private saveToken(token: string): void {
+  localStorage.setItem('token', token)
+}
 private handlerError(err): Observable<never>{
   let errorMessage = 'An error ocurred retrienving data';
   if(err){
