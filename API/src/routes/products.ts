ae1ec39cac3dbf47b1649346a704a2a0afe167fa
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2023-01-31 19:10:36 -0600

    cambios

--- API/src/routes/products.ts
+++ API/src/routes/products.ts
@@ -1,16 +1,15 @@
 import { Router } from "express";
-import AuthController from "../controller/AuthController";
 import { ProductsController } from './../controller/Products.Controller';
 
 
 
 const router = Router();
 
-//products
-router.get('/products', ProductsController.getAll)
+//products all products
+router.get('/products', ProductsController.getAll);
 
 //CreateProduct
 
-router.post('/products', ProductsController.new)
+router.post('/products', ProductsController.new);
 
 export default router;
