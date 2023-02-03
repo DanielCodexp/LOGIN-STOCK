276a7e3a1abc99b01d16b6251ed64574278f7006
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2023-02-03 13:05:06 -0600

    backup-bd

--- tools/final_tbrevisar.sql
+++ tools/final_tbrevisar.sql
@@ -16,32 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user`
+-- Table structure for table `tbrevisar`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `tbrevisar`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE `tbrevisar` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updateAT` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `cCodPrd` varchar(255) NOT NULL,
+  `dtFecReg` varchar(255) NOT NULL,
+  `cCvePer` varchar(255) NOT NULL,
+  `nInvAPrd` int NOT NULL,
+  `nRevPrd` tinyint NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_78a916df40e02a9deb1c4b75ed` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `IDX_48abe7f8e0d121ea8b796846da` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `tbrevisar`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (4,'super123','$2a$10$U2UXeOdZeZVE3DWpra25Hu7eyQTqhRkeIWkbje/rz8gUhYPd9Dyj2','admin','2023-01-30 10:46:35.637071','2023-01-30 12:18:16.000000'),(5,'batmans','$2a$10$YehRwVzSsWeesVKNMQYZfusvAkKcEz3axg7LfwXgYPN9DqipIAmbS','reader','2023-01-30 12:14:33.650380','2023-01-30 13:35:06.000000'),(7,'daniel','$2a$10$Hy70WSNgxchPHBCHnoulEurD4YB5yIsud.EoG.gRxNyZKClFWf1US','admin','2023-01-30 12:40:57.175469','2023-01-30 12:40:57.175469'),(8,'daniel123','$2a$10$YcZBadxWpqWeaD0DHVRuCOB9J9lhdr/3Iy7TkvEM8qjtG0eYtDIGy','admin','2023-01-30 12:44:27.204304','2023-01-30 12:44:27.204304'),(9,'perez123','$2a$10$YcZBadxWpqWeaD0DHVRuCOB9J9lhdr/3Iy7TkvEM8qjtG0eYtDIGy','admin','2023-01-30 12:44:27.204304','2023-01-30 12:44:27.204304');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `tbrevisar` WRITE;
+/*!40000 ALTER TABLE `tbrevisar` DISABLE KEYS */;
+INSERT INTO `tbrevisar` VALUES (1,'2','2023-02-02T17:20:14.074Z','A2075',2,0),(2,'BAN-722-01','2023-02-02T17:39:37.290Z','A2075',2,0),(3,'BAN-722-01','2023-02-02T17:42:27.491Z','A2075',8,0),(4,'BAN-722-01','2023-02-02T17:46:56.901Z','A2075',43111,0),(5,'BAN-722-01','2023-02-02T17:47:15.277Z','A2075',43111,0),(6,'BAN-722-01','2023-02-02T17:47:54.898Z','A2075',2,0),(7,'BAN-722-01','2023-02-02T17:51:47.683Z','A2075',1080045,0),(8,'BAN-722-01','2023-02-02T18:15:52.347Z','A2075',43111,0),(9,'BAN-722-01','2023-02-02T18:20:17.035Z','A2075',43111,0),(10,'TU-1172-15','2023-02-03T17:52:48.987Z','A2075',2,0);
+/*!40000 ALTER TABLE `tbrevisar` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
