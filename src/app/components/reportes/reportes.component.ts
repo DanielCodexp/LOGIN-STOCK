518e24fe15b9f1398beb9d88df8607c1cb36f250
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2023-02-07 14:36:44 -0600

    cambios

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -8,6 +8,7 @@ import { Reports } from '../../interfaces/reports';
 import { Router } from '@angular/router';
 import { StockService } from 'src/app/services/stock.service';
 import * as html2pdf from 'html2pdf.js';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
@@ -16,30 +17,41 @@ import * as html2pdf from 'html2pdf.js';
 export class ReportesComponent implements OnInit {
 
   public reports: Reports[] = [];
-  pModificar: Products = {
-    cCodPrd: '',
-    cDesPrd: '',
-    cPosPrd: '',
-    cDesUM: '',
-  };
+  // pModificar: Products = {
+  //   cCodPrd: '',
+  //   cDesPrd: '',
+  //   cPosPrd: '',
+  //   cDesUM: '',
+  // };
   pageSize = 15;
   desde: number = 0;
   hasta: number = 15;
   public imprimir = false;
+
+  columnas: string[] = ['codigo', 'clave Personal', 'fecha de reporte', 'Existencia Real','Revisado'];
 
   constructor(
     private ProductsService: ProductsService,
     private router: Router,
     private stock: StockService
   ) {}
-
+  dataSource:any;
   ngOnInit(): void {
     this.getReports();
+    this.dataSource = new MatTableDataSource(this.reports);
+
+
+  }
+
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase();
   }
 
   getReports() {
     this.stock.getReport().subscribe((res) => {
       this.reports = <any>res;
+      console.log(this.reports)
     });
   }
 
@@ -86,3 +98,7 @@ export class ReportesComponent implements OnInit {
     html2pdf().from(element).set(opt).save();
   }
 }
+function filtrar(event: Event | undefined, Event: { new(type: string, eventInitDict?: EventInit | undefined): Event; prototype: Event; readonly AT_TARGET: number; readonly BUBBLING_PHASE: number; readonly CAPTURING_PHASE: number; readonly NONE: number; }) {
+  throw new Error('Function not implemented.');
+}
+
